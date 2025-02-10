abb5f0fe453d8b867f7395a9ccf1fde1f04fcd5e
Shrey Kothari <shk29171112@gmail.com>
2025-02-10 22:04:05 +0530

    Footer Made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Login from './Pages/Login'
+import Footer from './Components/Footer'
 
 function App() {
 
   return (
+    <div className="overflow-hidden">
       <Login />
+      <Footer />
+    </div>
   )
 }
 
